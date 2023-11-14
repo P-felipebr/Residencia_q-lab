c544bf6b33ae38350b1f6d442b40c603182320b9
Matheus <matheus.arcelino@souunit.com.br>
2023-11-14 17:12:45 -0300

    newsletter com erro

--- q-lab/backend/index.js
+++ q-lab/backend/index.js
@@ -1,11 +1,25 @@
+import {engine} from 'express-handlebars'
 import express  from 'express';
 import cors from 'cors';
-import UserRoutes from "./routes/UserRoutes.js";
 import coon from './db/coon.js' 
 
-coon();
+//import rotas
+import UserRoutes from "./routes/UserRoutes.js";
+import NewsRoutes from "./routes/newsRouter.js";
 
+
+coon();
 const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+//read body
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
 
 //config json response
 
@@ -14,11 +28,13 @@ app.use(express.json());
 //solve cors
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 
-//public folder for images ;
+//public folder for images ; 
 
 //Routes 
 
 app.use('/users', UserRoutes);
+
+app.use('/newsletters', NewsRoutes); 
 
 app.listen(5000, function(){
     console.log("Servidor Online!!");
---
+++ q-lab/backend/routes/newsRouter.js
@@ -0,0 +1,35 @@
+import  express  from 'express';
+const router = express.Router();
+
+import newsletterController from '../controllers/newsletterController.js';
+
+router.route('/create')
+.get((req, res) => newsletterController.createNewsletter(req, res));
+
+
+router.route('/create')
+.post((req, res) => newsletterController.create(req, res));
+
+
+router.route('/remove/:id')
+.post((req, res) => newsletterController.deleteNewsletter(req, res));
+
+
+router.route('/edit')
+.post((req, res) => newsletterController.editNewsletterUpdate(req, res));
+
+
+router.route('/edit/:id')
+.get((req, res) => newsletterController.editNewsletter(req, res));
+
+
+router.route('/:id') 
+.get((req, res) => newsletterController.getNewsletter(req, res));
+
+
+router.route('/')
+.get((req, res) => newsletterController.showNewsletter(req, res));
+
+
+
+export default router;
